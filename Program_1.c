6e5240843460285695bd55d31bb259d42b2f5d23
sudhanvaif <79088359+sudhanva-if@users.noreply.github.com>
2021-02-19 11:26:00 +0000

    counting of ovels takes place

--- Program_1.c
+++ Program_1.c
@@ -4,10 +4,24 @@
 
 int main()
 {
-	char str[10];
+	char str[5];
+  int i,count=0;
 	scanf("%s", str);
 
 	//Write your code here
 
+
+for(i=0;str[i]!='\0';i++)
+{
+  if(str[i]=='a'||
+  str[i]=='e' || 
+  str[i]=='i' || 
+  str[i]=='o'||
+   str[i]=='u')
+{
+  count++;
+}
+}
+printf("%d",count);
 	return 0;
 }
