12496825c8e4475401b65ea7e24b6318b90e2c08
sudhanvaif <79088359+sudhanva-if@users.noreply.github.com>
2021-02-19 12:19:23 +0000

    SUDHANVA & MEGHA

--- Program_2.c
+++ Program_2.c
@@ -9,6 +9,21 @@ int main()
 	scanf("%s", str);
 
 	//Write your code here
-
+  char rev[10];
+  int i,j,k,count=0;
+  for(i=0;str[i]!='\0';i++ )
+  {
+    count++;
+  }
+  k=count-1;
+for(j=0;j<count;j++)
+{
+rev[j]=str[k];
+k--;
+}
+rev[j]='\0';
+  printf("%s",rev);
+  printf("\n");
+ 
 	return 0;
 }
